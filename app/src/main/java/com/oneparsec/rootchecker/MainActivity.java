cfc4a18dcddef6c37c908ef47c2676bed5c087c0
Alexandr Simonov <asimonov1998@gmail.com>
2021-10-02 13:11:03 +0300

    Update MainActivity.java

--- app/src/main/java/com/oneparsec/rootchecker/MainActivity.java
+++ app/src/main/java/com/oneparsec/rootchecker/MainActivity.java
@@ -16,7 +16,7 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    String version = "1.1";
+    String version = "1.1.1";
 
     private static boolean checkRootMethod1() {
         String[] paths = { "/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
